f77e7342806ad317b3c2cdc2279c84ff6e85c960
Ilya Kovalenko <ilyacoding@yandex.com>
2019-03-24 12:51:56 +0300

    Fix rubocop

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,34 +1,26 @@
 class AnswersController < ApplicationController
-  before_action :set_answer, only: [:show, :edit, :update, :destroy]
+  before_action :set_answer, only: %i(show edit update destroy)
 
-  # GET /answers
-  # GET /answers.json
+  load_and_authorize_resource
+
   def index
     @answers = Answer.all
   end
 
-  # GET /answers/1
-  # GET /answers/1.json
-  def show
-  end
+  def show; end
 
-  # GET /answers/new
   def new
     @answer = Answer.new
   end
 
-  # GET /answers/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /answers
-  # POST /answers.json
   def create
     @answer = Answer.new(answer_params)
 
     respond_to do |format|
       if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
+        format.html { redirect_to @answer, notice: "Answer was successfully created." }
         format.json { render :show, status: :created, location: @answer }
       else
         format.html { render :new }
@@ -37,12 +29,10 @@ class AnswersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /answers/1
-  # PATCH/PUT /answers/1.json
   def update
     respond_to do |format|
       if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
+        format.html { redirect_to @answer, notice: "Answer was successfully updated." }
         format.json { render :show, status: :ok, location: @answer }
       else
         format.html { render :edit }
@@ -51,24 +41,21 @@ class AnswersController < ApplicationController
     end
   end
 
-  # DELETE /answers/1
-  # DELETE /answers/1.json
   def destroy
     @answer.destroy
     respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
+      format.html { redirect_to answers_url, notice: "Answer was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_answer
-      @answer = Answer.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def answer_params
-      params.require(:answer).permit(:content, :user_id, :question_id)
-    end
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:content, :user_id, :question_id)
+  end
 end
